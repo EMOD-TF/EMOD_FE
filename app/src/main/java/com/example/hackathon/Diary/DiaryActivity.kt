400efadfa34276ec7a9e37cad0f82b429f1571dd
geonheu <geonhui0987@gmail.com>
2025-08-17 21:15:22 +0900

    [Feat] #9 - finish to compose the UI

--- app/src/main/java/com/example/hackathon/Diary/DiaryActivity.kt
+++ app/src/main/java/com/example/hackathon/Diary/DiaryActivity.kt
@@ -1,15 +1,20 @@
 package com.example.hackathon.Diary
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowInsets
+import android.widget.Button
 import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import com.example.hackathon.HomeActivity
 import com.example.hackathon.R
 import com.example.hackathon.databinding.ActivityDiaryBinding
 
@@ -36,12 +41,38 @@ class DiaryActivity : AppCompatActivity() {
             }
         }
 
+        onBackPressedDispatcher.addCallback(this) {
+            showExitDiaryDialog()
+        }
+
     }
 
     fun setFragment(fragment: Fragment){
         supportFragmentManager.commit {
             replace(R.id.fl_diary, fragment, fragment::class.java.name)
         }
+    }
+
+    private fun showExitDiaryDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_exit_diary, null)
+        val dialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .create()
+
+        dialogView.findViewById<Button>(R.id.btn_yes).setOnClickListener {
+            // 홈 액티비티로 이동
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) // 기존 스택 제거
+            startActivity(intent)
+            dialog.dismiss()
+        }
+
+        dialogView.findViewById<Button>(R.id.btn_no).setOnClickListener {
+            // 그냥 닫기
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
 //    override fun onBackPressed() {
